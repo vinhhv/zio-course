c53cae0ec6694567d0db9cf0263802471e56aa2c
Vinh Vu <vinhhv@uci.edu>
2024-01-30 12:47:42 -0800

    Promise exercises

--- src/main/scala/com/rockthejvm/part4coordination/Promises.scala
+++ src/main/scala/com/rockthejvm/part4coordination/Promises.scala
@@ -1,6 +1,7 @@
 package com.rockthejvm.part4coordination
 
 import com.rockthejvm.utils.*
+import zio.Exit.{Failure, Success}
 import zio.*
 
 object Promises extends ZIOAppDefault {
@@ -97,5 +98,77 @@ object Promises extends ZIOAppDefault {
     } yield ()
   }
 
-  def run = downloadFileWithRefPromise()
+  /**
+   * Exercises
+   * 1. Write a simulated "egg boiler" with two ZIOs
+   * - one increments a counter every 1s
+   * - one waits for the counter to become 10, after which it will "ring a bell"
+   *   
+   * 2. Write a "race pair"
+   * - use a Promise which can hold on Either[exit for A, exit for B]
+   * - start a fiber for each ZIO
+   * - on completion (with any status), each ZIO needs to complete that Promise
+   * - (hint: use a finalizer)
+   * - waiting on the Promise's value can be interrupted!
+   * - if the whole race is interrupted, interrupt the running fibers
+   */
+  def eggBoiler(): Task[Unit] = {
+    def countTo(ticks: Ref[Int], promise: Promise[Nothing, Unit]): UIO[Unit] =
+      for {
+        _     <- ZIO.sleep(1.second)
+        count <- ticks.updateAndGet(_ + 1)
+        _     <- ZIO.succeed(count).debugThread
+        _ <-
+          if (count >= 10) promise.succeed(())
+          else countTo(ticks, promise)
+      } yield ()
+
+    def awaitCounter(promise: Promise[Nothing, Unit]) =
+      promise.await.flatMap { _ => ZIO.succeed("Ding ding ding!").debugThread.unit }
+
+    for {
+      ticks   <- Ref.make(0)
+      promise <- Promise.make[Nothing, Unit]
+      _       <- countTo(ticks, promise) zipPar awaitCounter(promise)
+    } yield ()
+  }
+
+  def racePair[R, E, A, B](
+      zioa: => ZIO[R, E, A],
+      ziob: => ZIO[R, E, B]
+  ): ZIO[R, Nothing, Either[(Exit[E, A], Fiber[E, B]), (Fiber[E, A], Exit[E, B])]] =
+    ZIO.uninterruptibleMask { restore =>
+      for {
+        promise <- Promise.make[Nothing, Either[Exit[E, A], Exit[E, B]]]
+        fibA    <- restore(zioa).onExit(exita => promise.succeed(Left(exita))).fork
+        fibB    <- restore(ziob).onExit(exitb => promise.succeed(Right(exitb))).fork
+
+        result <- restore(promise.await).onInterrupt {
+          for {
+            interruptA <- fibA.interrupt.fork
+            interruptB <- fibB.interrupt.fork
+            _          <- interruptA.join
+            _          <- interruptB.join
+          } yield ()
+        }
+      } yield result match {
+        case Left(exitA)  => Left((exitA, fibB))
+        case Right(exitB) => Right((fibA, exitB))
+      }
+    }
+
+  val demoRacePair = {
+    val zioa = ZIO.sleep(1.second).as(1).onInterrupt(ZIO.succeed("1st interrupted").debugThread)
+    val ziob = ZIO.sleep(2.second).as(2).onInterrupt(ZIO.succeed("2nd interrupted").debugThread)
+
+    val pair = racePair(zioa, ziob)
+
+    pair.flatMap {
+      case Left((exita, fibb)) => fibb.interrupt *> ZIO.succeed("1st won").debugThread *> ZIO.succeed(exita).debugThread
+      case Right((fiba, exitb)) =>
+        fiba.interrupt *> ZIO.succeed("2nd won").debugThread *> ZIO.succeed(exitb).debugThread
+    }
+  }
+
+  def run = demoRacePair
 }
